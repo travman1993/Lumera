ccaede3fb56a3310654f46362ff6de00ba70548f
Travis Rodriguez <travisgolembiewski@gmail.com>
2026-05-23 22:52:07 -0400

    Handling creating and verifying JWT tokens.

---
+++ backend/app/auth/jwt.py
@@ -0,0 +1,33 @@
+import os 
+from datetime import datetime, timedelta, timezone
+from jose import JWTError, jwt
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+def create_access_token(data: dict) -> str:
+    payload = data.copy()
+
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    payload.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
+
+def verify_token(token: str) -> str | None:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithm=[ALGORITHM])
+
+        user_id: str = payload.get("sub")
+
+        if user_id is None:
+            return None
+        return user_id
+    
+    except JWTError:
+        return None
