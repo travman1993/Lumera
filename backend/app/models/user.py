b7bd64d7d922cc03a3cb45f82ada056ae99e6639
Travis Rodriguez <travisgolembiewski@gmail.com>
2026-05-23 22:30:01 -0400

    Define the users table in PostgreSQL using Python classes instead of raw SQL.

---
+++ backend/app/models/user.py
@@ -0,0 +1,36 @@
+from sqlalchemy import Column, String, Boolean, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from datetime import datetime, timezone
+import uuid
+
+# Import base class
+from app.database.db import Base
+
+class User(Base):
+    # Table Name
+    __tablename__ = "users"
+
+    # UUID
+    id = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+        nullable=False
+    )
+
+    email = Column(String, unique=True, index=True, nullable=False)
+
+    username = Column(String, unique=True, index=True, nullable=False)
+
+    # Hash version of password
+    hash_password = Column(String, nullable=False)
+
+    # Verified email
+    is_verified = Column(Boolean, default=False)
+
+    # Account Active or not
+    is_active = Column(Boolean, default=True)
+
+    # Auto timestamp
+    created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
